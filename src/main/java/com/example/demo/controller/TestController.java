8390387d86136f0a99e953e76e49eaf58267e458
陈浩 <chenhao@chenhaodeMacBook-Pro.local>
2021-01-06 20:45:16 +0800

    update

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -4,11 +4,17 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @RestController
 @RequestMapping("/api")
 public class TestController {
     @RequestMapping("hello")
     public String test(){
+        Random random=new Random();
+        boolean tag=random.nextBoolean();
+        Number number=(tag||!tag)?new Integer(3):new Long(1);
+        System.out.println(number);
         return "hello world";
     }
 }
